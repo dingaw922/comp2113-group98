d450c803e77614bae46b4c6be3c81e28f87971e2
dingaw922 <71168480+dingaw922@users.noreply.github.com>
2020-11-21 23:21:56 +0800

    Update input.h

--- input.h
+++ input.h
@@ -1,8 +1,11 @@
+// Filename: input.h 
+// Description: Header file for input.cpp
+// Referece: Goga Koreli, Snake, (2016), GitHub repository, https://github.com/gogakoreli/snake
+
 #ifndef _input_h
 #define _input_h
 
 #include "input.h"
-
 
 void input_init();
 void input_enter_off();
