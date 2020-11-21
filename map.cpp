998b3a92beb6ce8f609b2422a905545cfed94333
dingaw922 <71168480+dingaw922@users.noreply.github.com>
2020-11-21 23:36:54 +0800

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -4,20 +4,21 @@
 #include <iostream>
 #include <unistd.h>
 #include <iomanip>
+
 #include "map.h"
 #include "rocket.h"
 #include "debris.h"
-//#include "macros.h"
 
 using namespace std;
 
-//WIDTH = 51;
-//HEIGHT = 21;
+/* Define a 2-dimensional array to store the map */
 char map[height][width];
-char map_char = ' ';
-char map_divider = '|';
+char map_char = ' ';          
+char map_divider = '|';     // divider between two playspaces 
 
-void displayMap(char map[height][width]){
+/* function to print map*/
+void displayMap(char map[height][width])
+{
   for (int i = 0; i < height; i++){
     for (int j = 0; j < width; j++){
       cout << map[i][j] ;
@@ -26,7 +27,9 @@ void displayMap(char map[height][width]){
   }
 }
 
-void cleanMap(char map[height][width]){
+/* function to reset the map*/
+void cleanMap(char map[height][width])
+{
   for (int i = 0; i < height; i++){
     for (int j = 0; j < width; j++){
       if (j == 25) map[i][j] = map_divider;
@@ -35,12 +38,16 @@ void cleanMap(char map[height][width]){
   }
 }
 
-void updateRocket(char map[height][width], Rocket rocket){
+/* function to update the rocket position in the map */
+void updateRocket(char map[height][width], Rocket rocket)
+{
   char rocket_icon = rocket.avatar;
   map[rocket.y][rocket.x] = rocket_icon;
 }
 
-void updateDebris(char map[height][width], Debris debrisList[], int numdebris){
+/* function to update the debris position in the map */
+void updateDebris(char map[height][width], Debris debrisList[], int numdebris)
+{
   char debris_icon = '-';
   for (int i = 0; i < numdebris; i++){
     if (debrisList[i].y > height || debrisList[i].x > width) continue;
@@ -48,14 +55,24 @@ void updateDebris(char map[height][width], Debris debrisList[], int numdebris){
   }
 }
 
-void updateScore(char map[height][width], Rocket rocket1, Rocket rocket2){
+/* function to update the scores in the map */
+void updateScore(char map[height][width], Rocket rocket1, Rocket rocket2)
+{
     map[height - 1][rocket1.x] = '0' + (rocket1.score % 10);
     map[height - 1][rocket1.x - 1] = '0' + (rocket1.score / 10);
     map[height - 1][rocket2.x] = '0' + (rocket2.score%10);
     map[height - 1][rocket2.x - 1] = '0' +  (rocket2.score/10);
 }
 
-void updateMap(char map[height][width], Rocket rocket1, Rocket rocket2, int numdebris, Debris debrisList[]){
+/* function to update the map every PAUSELENGTH (1000 * 10 milliseconds) 
+1. clean map
+2. update rocket 1 and 2 position 
+3. update debris position 
+4. update scores
+5. print map
+*/
+void updateMap(char map[height][width], Rocket rocket1, Rocket rocket2, int numdebris, Debris debrisList[])
+{
   cleanMap(map);
   updateRocket(map, rocket1);
   updateRocket(map, rocket2);
