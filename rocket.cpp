eec7b3be10366b6f669d64c9f5b12d36df7af6c3
dingaw922 <71168480+dingaw922@users.noreply.github.com>
2020-11-21 23:26:26 +0800

    Update rocket.cpp

--- rocket.cpp
+++ rocket.cpp
@@ -3,17 +3,26 @@
 
 #include "rocket.h"
 #include "debris.h"
-//#include "macros.h"
 
-void Rocket::resetRocketPosition(){
+/* Function to reset the rocket's position when it collides with debris */
+void Rocket::resetRocketPosition()
+{
   y = height - 2;
 }
 
-void Rocket::addScore(){
+/* Function to add score to the runtime score when the rocket finishes a lap */
+void Rocket::addScore()
+{
   score++;
 }
-  //user 1 : w,s ; user 2: u, j;
-void Rocket::updateRocket1Position(char direction){
+
+/* Function to update the position of Rocket of User 2.
+Player 2 will control the rocket using key 'w' as up key and 's' as down key.
+Rocket position will be reset if it reach the top of the map (i.e. y- coordinate is 0)
+and one point will be added.
+*/ 
+void Rocket::updateRocket1Position(char direction)
+{
   if (direction == 'w' && y > 0) y--;
   else if (direction == 's' && y < (height -2)) y++;
   else if (y == 0) {
@@ -22,7 +31,13 @@ void Rocket::updateRocket1Position(char direction){
   }
 }
 
-void Rocket::updateRocket2Position(char direction){
+/* Function to update the position of Rocket of User 2.
+User 2 will control the rocket using key 'u' as up key and 'j' as down key.
+Rocket position will be reset if it reach the top of the map (i.e. y- coordinate is 0)
+and one point will be added.
+*/ 
+void Rocket::updateRocket2Position(char direction)
+{
   if (direction =='u' && y > 0) y--;
   else if (direction =='j' && y < (height -2)) y++;
   else if (y == 0) {
