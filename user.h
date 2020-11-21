bc1e70d6051fda00f4d392798d7e2504320be546
dingaw922 <71168480+dingaw922@users.noreply.github.com>
2020-11-21 21:36:21 +0800

    Update user.h

--- user.h
+++ user.h
@@ -1,13 +1,19 @@
-// user_profile.h
+// Fimename: user.h
+// Description: Header file for user.cpp
 
 #ifndef USER_H
 #define USER_H
 
-#include "user.h"
 #include <string>
 #include <vector>
+
+#include "user.h"
+
 using namespace std;
 
+/* Define a stuct User with 3 members and 4 member functions:
+name, score and avatar
+*/
 struct User{
   string name;
   int score;
@@ -17,13 +23,10 @@ struct User{
   void displayProfile(vector<User> &userlist);
   bool operator <(const User&a);
   void changeAvatar(char newAvatar, vector<User> & userlist);
-  //void addUser(vector<User> &userlist);
 };
 
+/* Functions related to User */
 bool user_exist(User a, vector<User> userlist);
-//void addUser(User, vector<User> userlist);
 void displayLeaderBoard(vector<User> & userlist);
-
-
 
 #endif
