aded692e62704d6c04414ffb16eb5372da82de9e
dingaw922 <71168480+dingaw922@users.noreply.github.com>
2020-11-21 21:26:21 +0800

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,11 +1,18 @@
+// Filename: Menu.cpp 
+// Description: Functions to display menu and to execute the menu 
+
 #include <iostream>
+
+#include "menu.h"
 #include "user.h"
 #include "game.h"
-#include "menu.h"
 #include "debris.h"
+
 using namespace std;
 
-bool checkChoiceValidity(int menuChoice){
+/* Function to check if the user's input is valid */
+bool checkChoiceValidity(int menuChoice)
+{
   while (menuChoice > 4 || menuChoice < 1){
     cout << "\nInvalid Choice\nPlease Enter Again: " << endl;
     return true;
@@ -13,8 +20,9 @@ bool checkChoiceValidity(int menuChoice){
   return false;
 }
 
-
-void displayRules(){
+/* Function to display the basic rules of the game */
+void displayRules()
+{
   cout << "*** Basic Rules ***" << endl;
   cout << "- Players will select th difficulty level before the game starts. \n";
   cout << "- There will be 3 difficulty levels such as 1, 2 and 3.\n";
@@ -31,7 +39,11 @@ void displayRules(){
   cout << "\n";
 }
 
-int displayHelpMenu(){
+/* Function to display the sub menu "Help Menu" 
+and prompt user's input and return user's input
+*/
+int displayHelpMenu()
+{
   cout << "*** Help Menu ***" << endl;
   cout << "1. User Profile" << endl;
   cout << "2. Change Rocket Avatar" << endl;
@@ -44,7 +56,10 @@ int displayHelpMenu(){
   return helpChoice;
 }
 
-void displayMenu(User user1, User user2,vector<User> &userlist, Debris debrisList[], int numdebris){
+/* Function to display the main menu and execute accordingly */
+void displayMenu(User user1, User user2,vector<User> &userlist, Debris debrisList[], int numdebris)
+{  
+  // Main Menu Content and prompt user input
   cout << "*** Main Menu ***" << endl;
   cout << "1. Start Game" << endl;
   cout << "2. Leader Board" << endl;
@@ -54,6 +69,15 @@ void displayMenu(User user1, User user2,vector<User> &userlist, Debris debrisLis
   int menuChoice;
   cin >> menuChoice;
   cout << "\n";
+  
+  /*Execution of user input 
+  Choices: 
+  1. Enter the space race game
+  2. Leader Board of all the users will be displayed
+  3. Sub menu will be displayed
+  4. Exit the whole game
+  After the execution of completes, main menu will be displayed, unless 4. Quit
+  */
   switch (menuChoice){
     case 1:
       game(debrisList, user1, user2, userlist, numdebris);
@@ -67,7 +91,17 @@ void displayMenu(User user1, User user2,vector<User> &userlist, Debris debrisLis
       cout << "*** Bye Bye! ***" << endl;
       break;
     case 3:
-      int helpChoice = displayHelpMenu();
+      int helpChoice = displayHelpMenu();             // display sub menu
+      
+      /* Execution of user input  
+      1. Users' profiles will be displayed one after another 
+      2. Facilitate the change of rocket's avatar 
+      - user wil be prompted to input 1 / 2 to identify which user's avatar will be changed
+      - Option 0 is provided to allow user to return to main menu 
+      3. Basic rules of the game will be displayed 
+      4. Back to main menu option 
+      After the execution of completes, main menu will be displayed, unless option 4 or any other keys.
+      */
       switch (helpChoice){
         case 1:
           cout << "*** Users' Profile ***" << endl;
@@ -77,7 +111,8 @@ void displayMenu(User user1, User user2,vector<User> &userlist, Debris debrisLis
           user2.displayProfile(userlist);
           displayMenu(user1, user2,userlist, debrisList, numdebris);
           break;
-        case 2: // players will have to specify which player wants to change avataer
+          
+        case 2: 
           cout << "*** Change Rocket Avatar ***" << endl;
           cout << "Player 1 Or Player 2 Wants To Change Rocket ? (1/2)" << endl;
           cout << "Press 0 To Return To Main Menu" << endl;
@@ -97,10 +132,12 @@ void displayMenu(User user1, User user2,vector<User> &userlist, Debris debrisLis
           cout << "\n" ;
           displayMenu(user1, user2,userlist, debrisList, numdebris);
           break;
+        
         case 3:
           displayRules();
           displayMenu(user1, user2,userlist, debrisList, numdebris);
           break;
+        
         default:
           displayMenu(user1, user2,userlist, debrisList, numdebris);
           break;
